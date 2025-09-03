a153459c15f1a06ac828a0d93d3d16c774c93b4d
yulinhuang <yulinhuang23@gmail.com>
2025-09-03 08:03:47 +0000

    Update init script for trainees

--- sspcloud/3_RAG/init-trainees-RAG.sh
+++ sspcloud/3_RAG/init-trainees-RAG.sh
@@ -3,7 +3,8 @@
 WORK_DIR="/home/onyxia/work"
 GITHUB_REPOSITORY="https://github.com/aiml4os/WP10_tutorial_text_classification.git"
 GITHUB_BRANCH="main"
-NOTEBOOK_DOWNLOAD_URL="https://aiml4os.github.io/WP10_tutorial_text_classification/notebooks/chapters/model_training/fine-tuning-bert-example.out.ipynb"
+NOTEBOOK_DOWNLOAD_URL="https://aiml4os.github.io/WP10_tutorial_text_classification/notebooks/chapters/3_RAG/rag_classification.out.ipynb"
+DATA_DOWNLOAD_URL="https://github.com/AIML4OS/WP10_tutorial_text_classification/blob/main/chapters/3_RAG/data/NACE_Rev2.1_Structure_Explanatory_Notes_EN.xlsx"
 BUCKET_PATH="s3/yulinhuang/tutorial/bert/"
 DEST_DIR="$HOME/work/models/localsave/bert"
 DEST_FILE="$DEST_DIR/$(basename "$BUCKET_PATH")"
@@ -15,18 +16,16 @@ git clone --depth 1 --branch $GITHUB_BRANCH $GITHUB_REPOSITORY temp
 
 # Install dependencies in system env
 uv pip install -r temp/pyproject.toml --system
-mkdir -p data/raw
-uv run python temp/sspcloud/data_ingestion.py
 rm -rf temp
 
-# Download the dataset
-echo "Downloading $BUCKET_PATH to $DEST_FILE..."
-mkdir -p $DEST_DIR
-mc cp -r "$BUCKET_PATH" "$DEST_DIR"
+# Download the excel data
+echo "Downloading excel"
+mkdir data/
+curl -L $DATA_DOWNLOAD_URL -o "${WORK_DIR}/data/NACE_Rev2.1_Structure_Explanatory_Notes_EN.xlsx"
 
 # Download the notebook directly using curl
 echo $NOTEBOOK_DOWNLOAD_URL
-curl -L $NOTEBOOK_DOWNLOAD_URL -o "${WORK_DIR}/WP10_tutorial_finetune_bert.ipynb"
+curl -L $NOTEBOOK_DOWNLOAD_URL -o "${WORK_DIR}/WP10_tutorial_rag_classification.ipynb"
 
 # Ensure Quarto extension is up to date
 code-server --install-extension quarto.quarto
